c6d880d02f7cd0666174cb7330d25867a0e6bf1c
madhu14041992 <madhuyerra111@gmail.com>
2018-01-23 19:46:00 +0530

    My Project Setup Completed Successfully....

---
+++ shoponlinebackendproject/src/main/java/shop/web/shoponlinebackendproject/App.java
@@ -0,0 +1,13 @@
+package shop.web.shoponlinebackendproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
